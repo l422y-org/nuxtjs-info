67839b72a4e3799444ed38d58d309e886f48f310
Larry W <l422y@l422y.com>
2023-08-16 05:52:34 -0400

    Add build hook to fetch and save release data from GitHub API

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+
+import path from "path"
+import * as fs from "fs"
+
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+    devtools: {enabled: true},
+    hooks: {
+        "build:before": async () => {
+            try {
+                const response = await fetch("https://api.github.com/repos/nuxt/nuxt/releases")
+                if (!response.ok) {
+                    throw new Error("Failed to fetch releases")
+                }
+                const data = await response.json()
+                const outputPath = path.resolve(__dirname, "static/releases.json")
+                fs.writeFileSync(outputPath, JSON.stringify(data, null, 2))
+                console.log("Fetched and saved release data!")
+            } catch (error) {
+                console.error("Error fetching releases:", error)
+            }
+        }
+    }
+
 })
