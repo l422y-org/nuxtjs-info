58cbf416828ebc5f4d653409af4b5c74d8e2e443
Larry W <l422y@l422y.com>
2023-08-19 03:44:09 -0400

    Update layout, style, and structure for NuxtJS release overview page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,28 @@
 <template>
     <div>
-        <NuxtPage />
+        <NuxtPage/>
     </div>
 </template>
+<style lang="scss">
+body {
+  font-family: "Roboto", sans-serif;
+}
+</style>
+<script lang="ts" setup>
+useHead({
+    title: "NuxtJS Releases Overview",
+    meta: [
+        {
+            key: "description",
+            name: "description",
+            content: "NuxtJS Releases Overview"
+        }
+    ],
+    link: [
+        {
+            rel: "stylesheet",
+            href: "https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
+        }
+    ]
+})
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,22 @@
 <template>
     <div>
         <h1>GitHub Releases for nuxt/nuxt</h1>
-        <table v-if="releases && releases.length">
-            <thead>
-                <tr>
-                    <th>Release Name</th>
-                    <th>Published At</th>
-                    <th>Changes</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="release in releases" :key="release.id">
-                    <td>{{ release.name }}</td>
-                    <td>{{ release.published_at }}</td>
-                    <td class="body">
-                        <div v-html="micromark(release.body,'utf-8')"></div>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+        <div v-if="releases && releases.length" class="releases">
+            <header>
+                <div>Release / Published At</div>
+                <div>Changes</div>
+            </header>
+            <div v-for="release in releases" :key="release.id" class="release">
+                <div class="release__col1">
+                    <span class="release__name">{{ release.name }}</span>
+                    <small class="release__date">{{ release.published_at }}</small>
+                </div>
+
+                <div class="release__body">
+                    <div v-html="micromark(release.body,'utf-8')"></div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -39,13 +37,78 @@ const releases: Ref<Release[]> = ref(releasesData)
 
 </script>
 
-<style scoped>
-tr td {
-    padding-bottom: 2rem;
+<style lang="scss">
+
+.releases {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  gap: 0;
+
+  > * {
+    display: grid;
+    width: 100%;
+    max-width: 640px;
+    grid-template-columns: 6rem 1fr;
+    gap: 1rem;
+
+  }
+
 }
 
-.body {
-    white-space: pre-wrap;
-    max-width: 640px;
+.release {
+  border: 1px solid black;
+
+  &__name {
+    font-weight: bold;
+    display: block;
+  }
+
+  &__body {
+    padding: 1rem;
+
+    blockquote {
+      position: relative;
+      display: block;
+      margin-left: 0;
+      padding: 0.5rem 0.5rem 0.5rem 1rem;
+      text-align: center;
+      border-radius: .2rem;
+      background-color: #eee;
+
+      &:before {
+        position: absolute;
+        top: .5rem;
+        bottom: .5rem;
+        left: .5rem;
+        width: .25rem;
+        content: "";
+        background-color: gold;
+      }
+    }
+
+    p {
+      margin: 0;
+      padding: 0;
+    }
+
+    h2, h3 {
+      padding: 0;
+      margin-inline-end: 0;
+    }
+  }
+
+  &__col1 {
+    display: flex;
+    flex-direction: column;
+    padding: .5rem;
+  }
+
+  &__date {
+    font-size: 0.8rem;
+    display: block;
+  }
+
+
 }
 </style>
